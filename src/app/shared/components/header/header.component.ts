f072e89564fe61ac1918e31b88bb8df79704c8b6
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2020-06-20 13:37:58 -0500

    Se añadio el menu se adapta a dispositivos moviles

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  menuButton: HTMLElement;
+  contenedorEnlaces:HTMLElement;
+  toggle:boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.menuButton = document.getElementById('contenedor-btnMenu');
+    this.contenedorEnlaces = document.getElementById('enlaces');
   }
 
+  openMenu(): void{
+    console.log(`%cFue cliqueado!`, 'color: yellow;')
+
+    if(this.toggle){
+      this.contenedorEnlaces.classList.remove('dos');
+      this.contenedorEnlaces.className = ('enlaces uno');
+      this.toggle = false;
+      return;
+    }
+
+    if(!this.toggle){
+      this.contenedorEnlaces.className = ('dos');
+      this.toggle = true;
+      console.log(`%cManejo de error!
+      Estado: ${this.toggle}`, 'color: red;')
+      return;
+    }
+
+  }
 }
